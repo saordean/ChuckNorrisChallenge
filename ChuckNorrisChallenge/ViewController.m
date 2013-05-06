d106dbd4f59be54937d0d55390fe1cac53d1dbef
Jerry Taylor Kendrick <saordean@Deananh-MacBook-Pro.local>
2013-05-05 21:24:49 -0500

    Initial Commit

---
+++ ChuckNorrisChallenge/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChuckNorrisChallenge
+//
+//  Created by JerryTaylorKendrick on 5/5/13.
+//  Copyright (c) 2013 DeanAMH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ChuckNorrisChallenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChuckNorrisChallenge
+//
+//  Created by JerryTaylorKendrick on 5/5/13.
+//  Copyright (c) 2013 DeanAMH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
